7f67d15963870673044c62d5d1dca42de65859eb
vrushabhmungelwar <89674295+vrushabhmungelwar@users.noreply.github.com>
2021-10-08 11:52:07 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,9 @@
+
+const changeColor = window.setInterval(()=> (document.getElementById("changeColor").style.backgroundColor = "teal"), 1000 * 2);
+
+
+const changeColor1 = window.setInterval(()=> (document.getElementById("changeColor").style.backgroundColor = "skyblue"), 1000 * 4);
+
+
+const changeColor2 = window.setInterval(()=> (document.getElementById("changeColor").style.backgroundColor = "violet"), 1000 * 6);
+
